1bc1eb6aa5709d78fae92b9cc57223bc9b02e0c9
Nathan Gartside <gar18014@byui.edu>
2022-10-22 23:52:07 -0600

    Changed champer windchill file

--- champer/jss/windchill.js
+++ champer/jss/windchill.js
@@ -1,8 +1,13 @@
-let temp = 49;
-let windspeed = 10;
-
 function setwindchill(temp, windspeed) {
-    //do the rest of the stuff
+    document.getElementById("chill").value = "N/A";
+    if (temp <= 50 && windspeed > 3) {
+        let chill = Math.round((35.74 + (0.6215 * temp)) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temp * Math.pow(windspeed, 0.16)));
+
+        document.getElementById("chill").textContent = chill;
+    }
+
+    document.getElementById("temp").innerHTML = `${temp}&#8457;`;
+    document.getElementById("speed").textContent = `${windspeed} mph`;
 }
 
 setwindchill(49, 10);
