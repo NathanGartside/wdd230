695f99b90605af2bed588125b83a3fd8f3d96520
Nathan Gartside <gar18014@byui.edu>
2022-11-11 21:37:16 -0700

    Added stuff

---
+++ chamber/jss/directory.js
@@ -0,0 +1,35 @@
+//.addEventListener('click', ()=>{});
+//Have one for list and grid
+//.style.opacity = "1/.5"; change so the view that isn't being used is half opacity
+
+
+
+
+const requestURL = './data/data.json';
+function displayCard(card) {
+    let cardview = document.querySelector("cardview");
+    let cardel = document.createElement("div");
+    cardel.innerHTML =
+        `<img src="${card.imgURL}" alt="">
+        <p>${card.address}</p>
+        <p>${card.phone}</p>
+        <p><a href="">${card.websiteURL}</a></p>`;
+
+    cardview.appendChild(childel);
+}
+
+function displayList(list) {
+
+}
+
+fetch(requestURL) //Request info
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);  // temporary checking for valid response and data parsing
+        let buisness = jsonObject['businesses'];
+        businesses.forEach(displayCard);
+        businesses.forEach(displayList);
+    });
+
