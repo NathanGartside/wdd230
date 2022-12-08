df6503352d82205c7ce4e9b2311e61e5de3f52d2
Nathan Gartside <gar18014@byui.edu>
2022-12-07 17:31:43 -0700

    Added header and footer

---
+++ temple/js/main.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+document.getElementsByClassName('lastTimeUpdate')[0].textContent = document.lastModified;
